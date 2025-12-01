b4781b1e10dfc5c461c628c041e2a805312d54fa
infobkchome-code <infobkchome@gmail.com>
2025-12-01 13:02:36 +0100

    Add ValoraTuViviendaPage component

---
+++ app/components/valora-tu-vivienda/page.tsx
@@ -0,0 +1,9 @@
+import ValoradorDetalle from "../components/ValoradorDetalle";
+
+export default function ValoraTuViviendaPage() {
+  return (
+    <main className="max-w-6xl mx-auto px-4 py-10 md:py-12">
+      <ValoradorDetalle />
+    </main>
+  );
+}
