d8ea641c6e4ab6419280315291c7fa6b1c9e1251
infobkchome-code <infobkchome@gmail.com>
2025-12-01 13:11:39 +0100

    Delete app/components/valora-tu-vivienda/page.tsx

--- app/components/valora-tu-vivienda/page.tsx
+++
@@ -1,9 +0,0 @@
-import ValoradorDetalle from "../components/ValoradorDetalle";
-
-export default function ValoraTuViviendaPage() {
-  return (
-    <main className="max-w-6xl mx-auto px-4 py-10 md:py-12">
-      <ValoradorDetalle />
-    </main>
-  );
-}
